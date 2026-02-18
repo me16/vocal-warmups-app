31e597bc6e81eb9b92d69b2347a08223bc5f1b5c
me16 <nicholasgriffith3@gmail.com>
2026-02-18 09:15:06 -0500

    modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vocal-warmups-app/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 3000,
-    open: true
-  }
+    open: true,
+  },
 });
